f3be4c453f0fe83fecaf387a636da4f84fcda5cf
zhouyiping <zhouyiping_love@126.com>
2024-09-21 14:59:32 +0800

    测试触发Jenkins从仓库拉取源码2

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 async function bootstrap() {
   console.log('process.env', process.env)
   console.log('测试触发Jenkins从仓库拉取源码')
+  console.log('测试触发Jenkins从仓库拉取源码2')
   try {
     // 初始化数据源连接
     await AppDataSource.initialize()
